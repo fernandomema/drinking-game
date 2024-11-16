c53cb37928aa5f9c347e7d6eef720c996d6af7b8
fernandomema <fernandomema@gmail.com>
2024-11-16 16:28:00 +0100

    Add modes

--- src/lib/modes.ts
+++ src/lib/modes.ts
@@ -10,4 +10,12 @@ export const modes: { [key: string]: Mode } = {
         name: 'Pre-Party',
         pickCards: (questions: Question[]) => questions.filter((question) => question.tags.includes('friends')).slice(0, 10),
     },
+    "best-friends": {
+        name: 'Best Friends',
+        pickCards: (questions: Question[]) => questions.filter((question) => question.tags.includes('+18-light')).slice(0, 10),
+    },
+    hot: {
+        name: 'Hot',
+        pickCards: (questions: Question[]) => questions.filter((question) => question.tags.includes('+18')).slice(0, 10),
+    },
 }
