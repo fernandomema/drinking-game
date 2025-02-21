b4e208d74dc1fbdc885a5bc76d9743907550b538
Fernando Merino <fernandomema@gmail.com>
2025-02-21 13:28:49 +0000

    fix redirect

---
+++ src/routes/(blog)/blog/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { goto } from "$app/navigation";
+    import { onMount } from "svelte";
+
+    onMount(async () => {
+        const browserLang = navigator.language;
+        if (browserLang.includes('es')) {
+            goto('/blog/es');
+        } else {
+            goto('/blog/en');
+        }
+    });
+</script>
--- src/routes/(blog)/blog/+server.ts
+++
@@ -1,8 +0,0 @@
-import { redirect } from "@sveltejs/kit";
-
-export const GET = async ({ fetch, params, request }) => {
-    const browserLang = request.headers.get('accept-language');
-    const lang = browserLang.includes('es') ? 'es' : 'en';
-    const url = lang === 'es' ? '/blog/es' : '/blog/en';
-    throw redirect(301, url);
-}
