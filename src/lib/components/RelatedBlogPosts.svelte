2a1f69374a067bb8c8f4e76854c2af9290616d6f
fernandomema <fernandomema@gmail.com>
2025-03-06 09:14:54 +0000

    enhance module

--- src/lib/components/RelatedBlogPosts.svelte
+++ src/lib/components/RelatedBlogPosts.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
-    export let relatedPosts: { title: string; slug: string; image: string }[] = [];
+    import type { PostData } from "$lib/types/PostData";
+
+    export let locale = 'en';
+    export let relatedPosts: PostData[] = [];
 </script>
 
 <div class="bg-white rounded-lg shadow p-6 mb-6 w-full">
     <h3 class="text-xl font-semibold mb-4 text-center">Posts Relacionados</h3>
     <ul class="space-y-4">
         {#each relatedPosts as post}
-            <li class="text-center">
-                <a href={`/blog/${post.slug}`} class="block hover:text-indigo-600 transition-colors">
-                    <img src={post.image} alt={post.title} class="w-20 h-20 rounded-full mx-auto mb-2" />
+            <li class="text-start">
+                <a href={`/blog/${locale}/${post.seoTag}`} class="block hover:text-indigo-600 transition-colors">
+                    <img src={post.image} alt={post.title} class="w-full rounded-lg mx-auto mb-2" />
                     <span class="block font-medium">{post.title}</span>
                 </a>
             </li>
