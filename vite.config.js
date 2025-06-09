117926e635fb7bbe95f61d83a415f5de72b8e9e8
Fernando Merino <fernandomema@gmail.com>
2025-06-09 13:06:03 +0200

    Increase workbox cache limit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,13 @@ import { defineConfig } from 'vite';
 import { SvelteKitPWA } from '@vite-pwa/sveltekit'
 
 export default defineConfig({
-	plugins: [sveltekit(), SvelteKitPWA({
-		manifestFilename: 'static/manifest.json',
-	})],
+        plugins: [
+                sveltekit(),
+                SvelteKitPWA({
+                        manifestFilename: 'static/manifest.json',
+                        workbox: {
+                                maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+                        },
+                }),
+        ],
 });
