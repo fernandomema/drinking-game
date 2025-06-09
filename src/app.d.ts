caa3a3f03c14f704892bf7df024340eb3daf8664
Fernando Merino <fernandomema@gmail.com>
2025-06-09 13:01:18 +0200

    feat(types): declare custom prototypes

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,22 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface Platform {}
-	}
+        namespace App {
+                // interface Error {}
+                // interface Locals {}
+                // interface PageData {}
+                // interface Platform {}
+        }
+
+        interface Array<T> {
+                shuffle(): T[]
+        }
+
+        interface String {
+                spintax(): string
+                bold(): string
+                underline(): string
+        }
 }
 
 export {};
