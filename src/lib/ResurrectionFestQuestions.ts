8cc2414c991d4ec691da6b50c7a9b331267bfaef
Fernando Merino <fernandomema@gmail.com>
2025-06-10 09:48:11 +0200

    Remove Resurrection Fest icon

---
+++ src/lib/ResurrectionFestQuestions.ts
@@ -0,0 +1,249 @@
+import { Question } from "./questions";
+
+export const ResurrectionFestQuestions: Question[] = [
+  {
+    locales: {
+      es: 'Si alguna vez has ido al Resurrection Fest, bebe {shots} tragos',
+      en: 'If you have ever been to Resurrection Fest, drink {shots} shots'
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: 'Nombra tu banda de metal favorita o bebe {shots} tragos',
+      en: 'Name your favorite metal band or drink {shots} shots'
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: '¿Cuál fue tu primer concierto de metal? Comparte la historia o bebe {shots} tragos',
+      en: 'What was your first metal concert? Share the story or drink {shots} shots'
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: 'Todos los que lleven camiseta negra beben {shots} tragos',
+      en: 'Everyone wearing a black shirt drinks {shots} shots'
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: '{player1} imita el grito gutural de tu vocalista favorito',
+      en: '{player1} imitate the growl of your favorite vocalist'
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: '¿Cuál es tu himno del metal? Cántalo o bebe {shots} tragos',
+      en: 'What is your metal anthem? Sing it or drink {shots} shots'
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: 'Todos los que hayan hecho headbanging hasta marearse beben {shots} tragos',
+      en: 'Anyone who has headbanged until dizzy drinks {shots} shots'
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: '{player1}, forma un pequeño mosh pit con {player2}',
+      en: '{player1}, start a small mosh pit with {player2}'
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: 'Si sabes tocar algún instrumento de metal, presúmelo ahora o bebe {shots} tragos',
+      en: 'If you can play a metal instrument, show it off or drink {shots} shots'
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: 'Todos los que tengan el pelo largo se reparten {shots} tragos',
+      en: 'Everyone with long hair distributes {shots} shots'
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: '¿Qué banda te gustaría ver en el próximo Resurrection Fest? Responde o bebe {shots} tragos',
+      en: 'Which band would you like to see at the next Resurrection Fest? Answer or drink {shots} shots'
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: 'Canta el estribillo de una canción de metal o bebe {shots} tragos',
+      en: 'Sing the chorus of a metal song or drink {shots} shots'
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: 'Elige a alguien para que haga air guitar contigo durante 10 segundos',
+      en: 'Pick someone to air guitar with you for 10 seconds'
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: 'Si conoces la diferencia entre black metal y death metal, reparte {shots} tragos',
+      en: 'If you know the difference between black metal and death metal, distribute {shots} shots'
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: '{player1} nombra tres bandas españolas de metal',
+      en: '{player1} name three Spanish metal bands'
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: 'Todos los que sepan quién es Lemmy beben {shots} tragos',
+      en: 'Anyone who knows who Lemmy is drinks {shots} shots'
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: 'Dinos tu festival de música favorito además del Resurrection Fest o bebe {shots} tragos',
+      en: 'Tell us your favorite music festival besides Resurrection Fest or drink {shots} shots'
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: '¿Cuál ha sido el concierto más épico que has vivido? Cuenta la historia',
+      en: 'What has been the most epic concert you\'ve attended? Tell the story'
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: 'Si alguna vez has hecho crowd surfing, reparte {shots} tragos',
+      en: 'If you\'ve ever crowd surfed, distribute {shots} shots'
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: '{player1} describe tu mejor recuerdo en un festival',
+      en: '{player1} describe your best festival memory'
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: 'Todos los que prefieran el metal clásico al moderno beben {shots} tragos',
+      en: 'Anyone who prefers classic metal over modern metal drinks {shots} shots'
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: '{player1}, arma el cuerno del metal con la mano y grita \'yeah\'',
+      en: '{player1}, throw the metal horns and shout \'yeah\''
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: 'Si has comprado merchandising en un concierto, bebe {shots} tragos',
+      en: 'If you have bought merch at a concert, drink {shots} shots'
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: 'Todos los que toquen un instrumento de cuerda reparten {shots} tragos',
+      en: 'Everyone who plays a string instrument distributes {shots} shots'
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: 'Cuenta la vez que más tarde llegaste a un concierto y bebe {shots} tragos',
+      en: 'Tell the time you arrived latest to a gig and drink {shots} shots'
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: '{player1}, tararea un riff mítico y deja que adivinen la canción',
+      en: '{player1}, hum a classic riff and let others guess the song'
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: 'Todos los que tengan tatuajes de música metal beben {shots} tragos',
+      en: 'Anyone with metal-related tattoos drinks {shots} shots'
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: '¿Qué grupo descubriste gracias al Resurrection Fest? Comparte o bebe {shots} tragos',
+      en: 'Which band did you discover thanks to Resurrection Fest? Share or drink {shots} shots'
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: 'Si nunca has estado en un pogo, bebe {shots} tragos',
+      en: 'If you have never been in a pit, drink {shots} shots'
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: '{player1}, invéntate el nombre de un grupo de metal ficticio',
+      en: '{player1}, invent a name for a fictional metal band'
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: 'Todos los que hayan hecho air drum alguna vez reparten {shots} tragos',
+      en: 'Anyone who has ever air drummed distributes {shots} shots'
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: '¿Cuál es la letra de metal más rara que recuerdas? Recítala o bebe {shots} tragos',
+      en: 'What is the weirdest metal lyric you remember? Recite it or drink {shots} shots'
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: '{player1}, cuenta cuál sería tu cartel ideal para un festival',
+      en: '{player1}, tell us what your ideal festival lineup would be'
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: 'Todos los que alguna vez han dormido en tienda de campaña en un festival beben {shots} tragos',
+      en: 'Anyone who has ever camped at a festival drinks {shots} shots'
+    },
+    tags: ['resurrectionFest']
+  },
+  {
+    locales: {
+      es: 'Si has hecho un viaje largo solo para ver un concierto, reparte {shots} tragos',
+      en: 'If you have travelled far just to see a gig, distribute {shots} shots'
+    },
+    tags: ['resurrectionFest']
+  }
+];
