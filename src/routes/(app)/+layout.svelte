5153e329c01e177b0fa98f39d1390814ad049fed
Fernando Merino <fernandomema@gmail.com>
2025-06-21 08:58:19 +0000

    feat: load CrazyGames SDK dynamically on app layout

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -2,11 +2,8 @@
     import { onMount } from 'svelte';
     import { fade, fly } from 'svelte/transition';
 	import { PUBLIC_SENTRY_DSN } from '$lib/config';
-import { OriginChecker } from '$lib/OriginChecker';
-import { page } from '$app/stores';
-
-	
-
+	import { OriginChecker } from '$lib/OriginChecker';
+	import { page } from '$app/stores';
 	import { App } from '@capacitor/app';
 	import { browser } from '$app/environment';
 	if (browser) {
@@ -15,12 +12,8 @@ import { page } from '$app/stores';
 	    });
 	}
 
-export let data
-
-let isCrazyGames = false;
-if (browser) {
-    isCrazyGames = OriginChecker.isCrazyGames();
-}
+	export let data
+	let isCrazyGames = false;
 
 	onMount(async () => {
 		const Sentry = await import('@sentry/capacitor'); 
@@ -34,14 +27,21 @@ if (browser) {
 			replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
 			replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 		});
+		isCrazyGames = OriginChecker.isCrazyGames();
+		if (isCrazyGames) {
+			// load the CrazyGames SDK: https://sdk.crazygames.com/crazygames-sdk-v3.js
+			const script = document.createElement('script');
+			script.src = 'https://sdk.crazygames.com/crazygames-sdk-v3.js';
+			script.onload = () => {
+				window.CrazyGames.SDK.init();
+			};
+			document.head.appendChild(script);
+		}
 	});
 </script>
 
 <svelte:head>
         <meta name="robots" content="noindex, nofollow" />
-        {#if isCrazyGames}
-            <script src="https://sdk.crazygames.com/crazygames-sdk-v3.js"></script>
-        {/if}
 </svelte:head>
 
 {#key data.url}
