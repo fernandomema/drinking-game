078f59c183043fff4fabb484da44dee425acd0e8
Fernando Merino <fernandomema@gmail.com>
2025-06-21 08:58:19 +0000

    feat: load CrazyGames SDK dynamically on app layout

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,22 +1,30 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
-        namespace App {
-                // interface Error {}
-                // interface Locals {}
-                // interface PageData {}
-                // interface Platform {}
-        }
+    namespace App {
+        // interface Error {}
+        // interface Locals {}
+        // interface PageData {}
+        // interface Platform {}
+    }
 
-        interface Array<T> {
-                shuffle(): T[]
-        }
+    interface Array<T> {
+        shuffle(): T[]
+    }
 
-        interface String {
-                spintax(): string
-                bold(): string
-                underline(): string
+    interface String {
+        spintax(): string
+        bold(): string
+        underline(): string
+    }
+    
+    interface Window {
+        CrazyGames: {
+            SDK: {
+                init: () => void;
+            }
         }
+    }
 }
 
 export {};
